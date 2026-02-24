552ae95dd685b49d3b988bd29202a588d28557d2
alaouih22-cyber <alaouih22@gmail.com>
2026-02-24 20:15:40 +0100

    Update service worker to cache additional assets

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('prayer-store').then((cache) => cache.addAll([
-      'index.html',
-      'manifest.json'
-    ]))
+const CACHE_NAME = 'ramadan-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn-icons-png.flaticon.com/512/2319/2319830.png',
+  'https://www.islamcan.com/audio/adhan/azan1.mp3'
+];
+
+// Installa il Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// Gestisce le richieste offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
