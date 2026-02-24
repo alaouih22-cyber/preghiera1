d4890078e850f797ebbcb60d4e4a31f50e41a705
alaouih22-cyber <alaouih22@gmail.com>
2026-02-24 22:38:51 +0100

    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,6 +1,22 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker installato');
+const CACHE_NAME = 'mushaf-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn-icons-png.flaticon.com/512/2319/2319830.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
