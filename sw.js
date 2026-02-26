d9b5310ca875ad90d0f1a2f51b832d56c28a66ea
alaouih22-cyber <alaouih22@gmail.com>
2026-02-26 10:59:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,19 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: event.data.text(),
-        icon: 'https://cdn-icons-png.flaticon.com/512/2619/2619277.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/2619/2619277.png'
-    };
-    event.waitUntil(self.registration.showNotification('Muslim Pro', options));
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('push', (event) => {
+    const data = event.data ? event.data.json() : { title: 'تنبيه الصلاة', body: 'حان الآن موعد الآذان' };
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: 'https://cdn-icons-png.flaticon.com/512/2619/2619277.png',
+            vibrate: [200, 100, 200],
+            badge: 'https://cdn-icons-png.flaticon.com/512/2619/2619277.png'
+        })
+    );
 });
