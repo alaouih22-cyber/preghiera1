7638b281ef3a98e6968ddf030987c117691894e6
alaouih22-cyber <alaouih22@gmail.com>
2026-02-26 23:04:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,15 @@
-const CACHE_NAME = 'muslim-pro-v2'; // Versione aggiornata
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/2798/2798007.png'
-];
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
 
-// Installazione e caching
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-});
-
-// Attivazione e pulizia vecchie cache
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
-  return self.clients.claim();
-});
-
-// Strategia: Network first con fallback su cache
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
-});
-
-// Gestione click sulle notifiche
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  event.waitUntil(
-    clients.matchAll({type: 'window'}).then(windowClients => {
-      if (windowClients.length > 0) {
-        return windowClients[0].focus();
-      }
-      return clients.openWindow('./');
-    })
-  );
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true })
+        .then(function(clientList) {
+            if (clientList.length > 0) {
+                clientList[0].focus();
+                clientList[0].postMessage({ playAdhan: true });
+            } else {
+                clients.openWindow('./');
+            }
+        })
+    );
 });
