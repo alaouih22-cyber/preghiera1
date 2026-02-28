f59f3deb8bdd1bc778d391bd99f4087259bd685f
alaouih22-cyber <alaouih22@gmail.com>
2026-02-28 23:33:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,65 @@
-const CACHE_NAME = 'muslim-pro-bastia-v2026';
-const ASSETS_TO_CACHE = [
-  'index.html',
-  'manifest.json',
-  '1000087707.png',
-  'https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3'
+const CACHE_NAME = "muslim-pro-bastia-v2026";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./1000087707.png"
 ];
 
-// Installazione: Salva i file necessari nella cache
-self.addEventListener('install', (event) => {
+// INSTALL
+self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-// Attivazione: Pulisce le vecchie versioni della cache
-self.addEventListener('activate', (event) => {
+// ACTIVATE
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
   );
-  return self.clients.claim();
+  self.clients.claim();
 });
 
-// Fetch: Serve i file dalla cache se offline
-self.addEventListener('fetch', (event) => {
+// FETCH (offline-first)
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
 
-// Gestione Notifiche Push
-self.addEventListener('notificationclick', (event) => {
+// PUSH NOTIFICATIONS
+self.addEventListener("push", event => {
+  let data = { title: "Muslim Pro", body: "È ora della preghiera" };
+
+  if (event.data) {
+    data = event.data.json();
+  }
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: "1000087707.png",
+      badge: "1000087707.png",
+      vibrate: [200, 100, 200],
+      tag: "prayer-notification",
+      renotify: true
+    })
+  );
+});
+
+// CLICK NOTIFICATION
+self.addEventListener("notificationclick", event => {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow('/')
+    clients.openWindow("./index.html")
   );
 });
